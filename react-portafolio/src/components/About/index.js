0f86e7cb6cde352e79af968d6117967f43fff62e
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-10-21 00:10:02 -0300

    Agrego cubo que gira

--- react-portafolio/src/components/About/index.js
+++ react-portafolio/src/components/About/index.js
@@ -2,6 +2,15 @@ import './index.scss'
 import AnimatedLeters from '../AnimatedLeters'
 import { useEffect, useState } from 'react';
 import Loader from 'react-loaders'
+import {
+  faLaravel,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const About = () =>{
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -25,6 +34,28 @@ const About = () =>{
                         Good knowledge in HTML, CSS and JavaScript. Recently graduated in Computer Engineering from the UBB with all the competencies of 
                         the career as orientation towards collaborative work, organizational management and a permanent commitment to learning. 
                     </p>
+                </div>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faLaravel} color="#DD0031" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+                    </div>
                 </div>
             </div>
             <Loader type="pacman" />
