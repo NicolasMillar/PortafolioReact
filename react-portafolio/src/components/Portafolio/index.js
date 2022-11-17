19be1451b346a39138f41591ad504c3c8613e281
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2022-11-17 14:11:21 -0300

    nuevos proyectos

--- react-portafolio/src/components/Portafolio/index.js
+++ react-portafolio/src/components/Portafolio/index.js
@@ -3,23 +3,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import pudu from '../../assets/images/pudu.png';
+import guia from '../../assets/images/guia2.png';
+import intranet from '../../assets/images/intranet.png';
 
 const imagenes = [
     pudu,
-    "https://picsum.photos/id/1025/200/400",
-    "https://picsum.photos/id/1029/200/400",
+    guia,
+    guia,
+    intranet,
 ];
 
 const titulos = [
     "Pudu",
-    "Titulo 2",
-    "Titulo 3",
+    "Java developer–LABCIM ",
+    "Full Stack Developer–LABCIM ",
+    "Intranet for a schoool",
 ];
 
 const Descripcion = [
     "Pudu is MVP of an App that tells nutrition value and environment impact of foods, \nhelping you reach your fitness goals while taking care of the planet. This App was \ndeveloped for a IBM Call For Code 2022 in team whit two Software Engineer.",
-    "Descripcion 2",
-    "Descripcion 3",
+    "I developed computer vision programs in Java using the OpenCV library, and then \ntransferred them to a Scorbot robotic arm, making it perform automated tasks with the \nhelp of number and symbol recognition. I developed educational guides and \ndocumentation for future developers.",
+    "I developed the frontend of a web application, where I mainly used JavaScript, HTML, CSS and Bootstrap.\nSome of the functionalities I developed are: PDF generator from data received by Django Rest Framework, \na tool that displays dynamic graphics and some frontend views.",
+    "this intranet specializes in facilitating the processes of registering/reviewing their annotations/ratings \nto users in a faster, more efficient and orderly manner."
 ];
 
 
@@ -41,7 +46,7 @@ const Portafolio = () => {
             <div className='container portafolio-page'>
                 <div className='text-zone'>
                     <div className='image'>
-                        <button onClick={previus}><FontAwesomeIcon  icon={faArrowLeft} color="#4d4d4e" size='3x' /></button>
+                        <button onClick={previus}><FontAwesomeIcon  icon={faArrowLeft} color="#ffd700" size='3x' /></button>
                         {imagenes.map((imagen,index) => {
                             return (
                                 <div>
@@ -60,7 +65,7 @@ const Portafolio = () => {
                                 </div>
                             );
                         })}
-                        <button onClick={next}><FontAwesomeIcon  icon={faArrowRight} color="#4d4d4e"  size='3x' /></button> 
+                        <button onClick={next} className="leftbutton"><FontAwesomeIcon  icon={faArrowRight} color="#ffd700"  size='3x' /></button> 
                     </div>
                 </div>
             </div>
